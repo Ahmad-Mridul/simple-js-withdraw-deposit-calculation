ffe64550b80558798947a9251b61bc99681d37d4
Ahmad-Mridul <mridulahmed262@gmail.com>
2022-05-28 08:55:49 +0600

    readme file added

---
+++ README.md
@@ -0,0 +1,14 @@
+<!-- 
+
+Sipmle js deposit withdraw calculator
+
+Here is the live site link: https://js-depo-withdraw-calc.netlify.app/
+
+username: admin@gmail.com
+password:secret
+
+
+
+
+
+ -->
--- js/banking.js
+++ js/banking.js
@@ -1,30 +1,29 @@
 function getInputValue(id){
     const inputField = document.getElementById(id);
     const inputText = inputField.value;
-    const inputAmount = parseInt(inputText);
+    const inputAmount = parseFloat(inputText);
     inputField.value='';
     return inputAmount;
 }
 function updateValue(id,inputAmount){
     const previousField  = document.getElementById(id);
     const previousText = previousField.innerText;
-    const previousAmount = parseInt(previousText);
+    const previousAmount = parseFloat(previousText);
     previousField.innerText = previousAmount +inputAmount;
-    
 }
 function balanceAmount(){
     const previousBalanceField = document.getElementById('balance');
     const previousBalanceText = previousBalanceField.innerText;
-    const previousBalance = parseInt(previousBalanceText);
+    const previousBalance = parseFloat(previousBalanceText);
     return previousBalance;
 }
 function updateBalance(inputAmount,isAdd){
     const previousBalanceField = document.getElementById('balance');
     const previousBalance = balanceAmount();
     if(isAdd){
-        previousBalanceField.innerText = inputAmount + previousBalance;
+        previousBalanceField.innerText = (inputAmount + previousBalance).toFixed(2);
     }else{
-        previousBalanceField.innerText = previousBalance-inputAmount;
+        previousBalanceField.innerText = (previousBalance-inputAmount).toFixed(2);
     }
 }
 document.getElementById('depo-btn').addEventListener('click',function(){
